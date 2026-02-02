f4006204cc85ebff67df385138eb940f7121d58a
Anijesh <anijeshkr@gmail.com>
2026-02-03 00:46:41 +0530

    Add weather forecast feature with modern UI redesign

--- apps/weather/services.py
+++ apps/weather/services.py
@@ -25,3 +25,27 @@ class WeatherService:
             return response.json()
         except requests.RequestException:
             return {"error": "weather service unavailable"}
+        
+    
+    FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast'
+
+    @staticmethod
+    def get_forecast(city):
+        if not API_KEY:
+            return {'error':"API_KEY not found"}
+        
+        params = {
+            'q':city,
+            'appid':API_KEY,
+            'units':'metric'
+        }
+        try:
+            response = requests.get(
+                WeatherService.FORECAST_URL,
+                params=params,
+                timeout =5
+            )
+            return response.json()
+        except requests.RequestException:
+            return {"error":"Weather service unavailable"}
+        
--- apps/weather/views.py
+++ apps/weather/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 
+from django.http import JsonResponse
+
 from .services import WeatherService
+
+from django.views.decorators.csrf import csrf_exempt # importing for the exempt of csrf in django
 
 # Create your views here.
 
+# this is used for exmpet csrf token of django "@csrf_exempt"
 def home(request):
     context ={}
     if request.method == "POST":
         city = request.POST.get('city')
         weather_data = WeatherService.get_current_weather(city)
+        forcast_weather = WeatherService.get_forecast(city)
         context={'city':city,
-                'weather':weather_data}
+                'weather':weather_data,
+                'forecast':forcast_weather}
     return render(request,'weather/home.html',context)
